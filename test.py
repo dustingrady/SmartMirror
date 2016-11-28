177af6404a8410fdd0d64e206141c13830891df3
Dustin Grady <dustin.grady@gmail.com>
2016-11-28 11:35:30 -0800

    Add files via upload
    Moved from threading to tkinters 'after' for updating.

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
+#Author: Scott Grether, Dustin Grady
+#Function: Create GUI with TKinter and set up widgets. Clock and weather that updates
+#Status: Working/ Tested
+
 import time
 import calendar
-import threading
 from weather import WeatherClass
 
 try:
@@ -14,57 +17,77 @@ time1 = time.localtime(time.time())
 
 class Application(Frame):
 
-    def createTime(self):
-        self.Time = Label(self.top_right, text=time.asctime(self.localtime), background='black', fg = 'white')
+    def createTime(self):#Create Time widget
+        self.Time = Label(self.top_right, fg='white', background='black', font=self.labelfont)
         self.Time.pack(side='right')
         self.Time.config(borderwidth=0)
         self.Time.config(highlightthickness=0)
 
-    def createCal(self):
-        self.Cal = Text(self.bottom_right,height=8,width=0,background='black', fg = 'white')
-        self.Cal.insert(INSERT, calendar.month(self.localtime[0], self.localtime[1]))
+    def createCal(self):#Create calendar widget
+        self.Cal = Text(self.bottom_left,height=7,width=0,background='black', fg='white')
+        self.Cal.insert(INSERT, calendar.month(time1[0], time1[1]))
         self.Cal.pack(side='left',fill='both',expand=True)
         self.Cal.config(borderwidth=0)
         self.Cal.config(highlightthickness=0)
 
+    def createImage(self):#Create weather image widget
+        self.imageWidget = Label(self.bottom_right, fg='white', background='black', font=self.labelfont)
+        self.imageWidget.pack(side="right")
+        self.imageWidget.config(borderwidth=0)
+        self.imageWidget.config(highlightthickness=0)
+
+    def createText(self):#Create weather text widget
+        self.textWidget = Label(self.bottom_right, fg='white', background='black', font=self.labelfont)
+        self.textWidget.config(bg='black', fg='white')
+        self.textWidget.config(borderwidth=0)#Get rid of 1px border
+        self.textWidget.config(highlightthickness=0)#Get rid of 1px border
+        self.textWidget.config(height=3, width=20)
+        self.textWidget.pack(expand=NO, fill=BOTH, side='right')
+
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.localtime = time.localtime(time.time())
+
+        self.labelfont = ('Courier', 20, 'bold')#Font for all the widgets
 
         self.main_container = Frame(master, background='black')
         self.main_container.pack(side='top',fill='both',expand=True)
-        master.minsize(width=1000,height=300)
+        master.minsize(width=1000,height=500)#Set window size
 
-        self.top_frame = Frame(self.main_container,background='black')
+        self.top_frame = Frame(self.main_container, background='black')
         self.top_frame.pack(side='top',fill='x',expand=False)
 
-        self.bottom_frame = Frame(self.main_container,background='yellow')
+        self.bottom_frame = Frame(self.main_container, background='black')
         self.bottom_frame.pack(side='bottom',fill='x',expand=False)
 
-        self.top_left = Frame(self.top_frame, background="pink")
+        self.top_left = Frame(self.top_frame, background='black')
         self.top_left.pack(side="left", fill="x", expand=True)
 
-        self.top_right = Frame(self.top_frame, background="blue")
+        self.top_right = Frame(self.top_frame, background='black')
         self.top_right.pack(side="right", fill="x", expand=True)
 
-        self.bottom_right = Frame(self.bottom_frame,background='red')
+        self.bottom_right = Frame(self.bottom_frame, background='black')
         self.bottom_right.pack(side='right',fill='x',expand=True)
 
-        self.bottom_left = Frame(self.bottom_frame,background='orange')
+        self.bottom_left = Frame(self.bottom_frame, background='black')
         self.bottom_left.pack(side='left',fill='x',expand=True)
 
+        #Create all thew widgets
         self.createTime()
         self.createCal()
+        self.createImage()
+        self.createText()
 
-        self.top_left_label = Label(self.top_left, text="Top Left")
+        self.top_left_label = Label(self.top_left, text="Top Left", fg='white', background='black')
         self.top_left_label.pack(side="left")
 
-        self.bottom_left_label = Label(self.bottom_left,text='Bottom Left')
-        self.bottom_left_label.pack(side='left')
+        #self.top_right_label = Label(self.top_right, text="Top Right")
+        #self.top_right_label.pack(side="right")
 
-root = Tk()
-root.title('Smart Mirror')
-app = Application(root)
+        #self.bottom_right_label = Label(self.bottom_right,text='Bottom Right', fg='white', background='black')
+        #self.bottom_right_label.pack(side='right')
+
+        #self.bottom_left_label = Label(self.bottom_left,text='Bottom Left')
+        #self.bottom_left_label.pack(side='left')
 
 #Display live time
 def tick():
@@ -73,25 +96,8 @@ def tick():
     if time2 != time1:
         time1 = time2
         app.Time.config(text=time.asctime(time1))
-    app.Time.after(200, tick)
+    app.Time.after(1000, tick)
 
-'''Initialize the weather widgets'''
-labelfont = ('Courier', 20, 'bold')
-
-imageWidget = Label(root)
-imageWidget.pack(side="right")
-imageWidget.config(borderwidth=0)
-imageWidget.config(highlightthickness=0)
-
-textWidget = Label(root)
-textWidget.config(bg='black', fg='white')
-textWidget.config(font=labelfont)
-textWidget.config(borderwidth=0)#Get rid of 1px border
-textWidget.config(highlightthickness=0)#Get rid of 1px border
-textWidget.config(height=3, width=20)
-textWidget.pack(expand=NO, fill=BOTH, side='right')
-
-#weatherClassObject = WeatherClass()#Create object of our WeatherClass()
 '''Draw weather status/icon'''
 def draw_Weather():
     global weatherClassObject
@@ -100,17 +106,18 @@ def draw_Weather():
     weatherImage = weatherClassObject.weatherImage
     weatherInfo = weatherClassObject.location + '\n' + str(weatherClassObject.currentWeather) + '\n' + str(weatherClassObject.currentTemperature + 'F')#Get description of weather/ temperature
 
-    imageWidget.config(image = weatherImage) #update image
-    textWidget.config(text = weatherInfo) #update text
+    app.imageWidget.config(image = weatherImage) #update image
+    app.textWidget.config(text = weatherInfo) #update text
 
-    print('Updating..')#Testing
-    print(weatherClassObject.currentWeather)#Testing
-    print(weatherClassObject.currentTemperature)#Testing
-    threading.Timer(10, draw_Weather).start()#Updates every x seconds
-
+    #print('Updating..')#Testing
+    #print(weatherClassObject.currentWeather)#Testing
+    #print(weatherClassObject.currentTemperature)#Testing
+    app.imageWidget.after(30000, draw_Weather)#update every x milliseconds
     #return weatherClassObject
 
-tick()
+root = Tk()
+root.title('Smart Mirror')
+app = Application(root)
+tick()#Initial call for clock ticking
 draw_Weather()#Initial call to get draw_Weather going
-
-app.mainloop()
+root.mainloop()
--- weather.py
+++ weather.py
@@ -4,7 +4,6 @@
 
 import pyowm
 import time
-import threading
 
 try:
     #python3
@@ -19,45 +18,37 @@ time1 = time.localtime(time.time())
 class WeatherClass():
     def __init__(self):
         owm = pyowm.OWM(API_KEY)
-        self.location = 'Dallas, TX, USA'
+        self.location = 'Santa Cruz, CA, USA'
         observation = owm.weather_at_place(self.location)
         weather = observation.get_weather()
 
-        '''Get Today's forecast'''
-        #self.todaysForecast = str(weather).split("status=", 1)[1].replace(">", "")#Search for status and remove ">" from the end
-
         '''Get Current Weather Conditions'''
-        self.currentWeather = str(weather.get_detailed_status())
+        self.currentWeather = str(weather.get_detailed_status().title())
 
         '''Get Current Temperature in Fahrenheit'''
         self.currentTemperature = weather.get_temperature(unit = 'fahrenheit')
         self.currentTemperature = str(self.currentTemperature).split("'temp':", 1)[1]#Get temperature, removing everything before it
         self.currentTemperature = str(self.currentTemperature[:3])#Remove everything except for the 2 digits we need
 
-        print(self.currentWeather)#Testing
-        print(self.currentTemperature)#Testing
-
-        #self.todaysForecast = str(self.todaysForecast)
-
         '''Determine Weather Icon'''
-        if "clouds" in self.currentWeather:
+        if "Clouds" in self.currentWeather:
             self.weatherImage = PhotoImage(file="Cloudy.png")
-        if "clear" in self.currentWeather:
+        if "Clear" in self.currentWeather:
             if(time1.tm_hour > 6) and (time1.tm_hour <18):#Between 6am and 6pm will be day
                 self.weatherImage = PhotoImage(file="ClearSkyDay.png")
             else:
                 self.weatherImage = PhotoImage(file="ClearSkyNight.png")
-        if "sunny" in self.currentWeather:
+        if "Sunny" in self.currentWeather:
             self.weatherImage = PhotoImage(file="ClearSkyDay.png")
-        if "rain" in self.currentWeather:
+        if "Rain" in self.currentWeather:
             self.weatherImage = PhotoImage(file="Rain.png")
-        if "thunder" in self.currentWeather:
+        if "Thunder" in self.currentWeather:
             self.weatherImage = PhotoImage(file="Thunder.png")
-        if "snow" in self.currentWeather:
+        if "Snow" in self.currentWeather:
             self.weatherImage = PhotoImage(file="Snow.png")
-        if "haze" in self.currentWeather:
+        if "Haze" in self.currentWeather:
             self.weatherImage = PhotoImage(file="Foggy.png")
-        if "fog" in self.currentWeather:
+        if "Fog" in self.currentWeather:
             self.weatherImage = PhotoImage(file="Foggy.png")
-        if "mist" in self.currentWeather:
+        if "Mist" in self.currentWeather:
             self.weatherImage = PhotoImage(file="Foggy.png")
